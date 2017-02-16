fee933fb57a3302048eab2c362c935a80ebb41a2
chinedu ofor <chinedu.ofor@andela.com>
2017-02-16 13:07:31 +0100

    edit karma file

--- karma.conf.js
+++ karma.conf.js
@@ -60,7 +60,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['C:\Program Files (x86)\Tooleat\Application\chrome.exe'],
 
 
     // Continuous Integration mode
